ddf3c9756393c6c746fccdd98f0ac22ad393428a
steven-wolfman <wolf@cs.ubc.ca>
2015-10-24 21:13:14 -0700

    Restructured directories to make it possible to run the game.

--- Prophecy/moreMethods.js
+++
@@ -1,8 +0,0 @@
-// Append methods and properties to an existing class.
-Foundation.appendToClass(GamesByEmail.TicTacToeGame,
-{
-   getHeaderHtml:function(resourceName)
-   {
-      return "hello world<br>"+Super.getHeaderHtml(resourceName);
-   }
-});
