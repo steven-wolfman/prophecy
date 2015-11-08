fd810be69978baed91a1922f915d1414f4a0e2ae
steven-wolfman <wolf@cs.ubc.ca>
2015-11-08 06:05:07 -0800

    Working on SimpleCost
    Not yet ready to pull into gh-pages branch.

--- Prophecy/Games/Prophecy/Data.js
+++ Prophecy/Games/Prophecy/Data.js
@@ -51,7 +51,8 @@ Foundation.createClass
      * resource. Instead, use the existing canonical resources
      * accessible via getResource.
      */
-    function(/* String */ name)
+    /* Parameters: String */
+    function(name)
     {
 	if (GamesByEmail.Data.Resource.getResource(name))
 	    throw new "attempt to create duplicate resource \"" + name + "\""
@@ -193,16 +194,18 @@ Foundation.createClass(
     null,
     GamesByEmail.Data.Base,
     /*
-     * Constructor. Takes a Map<Resource -> Integer> of initial levels
-     * of resources. The map is optional; any resource requested but
-     * not provided will be undefined. Any resource ADJUSTED when not
+     * Constructor. The initial levels of resources (which is an
+     * object where each field is a resource name and its value an
+     * integer amount) are optional; any resource requested but not
+     * provided will be undefined. Any resource ADJUSTED when not
      * provided defaults to starting at 0.
      *
-     * All mapped resources MUST be canonical resources.
+     * All provided resources MUST be canonical resources.
      *
      * Resource levels are constrained between the default limits of
      * DEFAULT_MIN (0) and DEFAULT_MAX(Number.POSITIVE_INFINITY).
      */
+    /* Parameters: Map<String -> Integer> */
     function(initialLevels)
     {
 	var levels = {};
@@ -341,39 +344,123 @@ describe("GamesByEmail.Data.Character", function() {
 // // Costs
 // // ============================================================
 
-// TODO: documentation and testing!!!
+/*
+ * A cost OR benefit in the game (e.g., it might cost 1 health and 1
+ * magic to use the Dark Mace, or you might receive up to 2 health
+ * from healing). In the long run, it would be nice to handle "up to",
+ * "at least", choices (e.g., heal 1 health or restore 2 magic), etc.
+ *
+ * This version is abstract.
+ */
 Foundation.createClass(
     "GamesByEmail.Data.Cost",
     null,
     GamesByEmail.Data.Base,
+    /*
+     * Constructor.
+     */
     function()
     {
-	// Constructor
+	// Nothing to be done.
     },
     {
 	// Virtual methods
 
-	// TODO: throw unimplemented?
-
-	// Given a Character, determine whether that character can pay
-	// this cost.  Returns true if the character can pay; else,
-	// returns the type of resources that causes the problem.
-	canPay:function(character) { },
-	pay:function(character) { },
-	addSummaryHtml:function(htmlBuilder) { },
-	addFullHtml:function(htmlBuilder) { },
-	isPositive:function() { },   // Note: all four possible combos are reasonable,
-	isNegative:function() { },   //       both true: mixed; both false: free
-	isComparable:function() { }, // If it is, then it can be compared resource-by-resource; 
-	                             // it should not be considered to dominate or be dominated by any resource
-	// For the moment, at least, returns a Boolean,
-	// but maybe best to test against true (... === true)?
-	dominates:function(cost)
+	/*
+	 * Determine whether character can pay this cost.  Returns
+	 * true if the character can pay; else, returns the type of
+	 * resources that causes the problem.
+	 */
+	canPay:
+	/* true or Resource */ function(/* Character */ character) {
+	    throw "unimplemented";
+	},
+	/*
+	 * Pay this cost on the given character. Produces the actual
+	 * cost paid as a SimpleCost.
+	 *
+	 * PRECONDITION: this.canPay(character) === true.
+	 */
+	pay:
+	/* SimpleCost */ function(/* Character */ character) {
+	    throw "unimplemented";
+	},
+	/*
+	 * Add a short version of this cost to the given htmlBuilder
+	 * (e.g., "mm.." might mean "two or more magic").
+	 *
+	 * Returns the same htmlBuilder.
+	 */
+	addSummaryHtml:
+	/* Foundation.StringBuilder */ function(/* Foundation.StringBuilder */ htmlBuilder) {
+	    throw "unimplemented";
+	},
+	/*
+	 * Add a full version of this cost to the given htmlBuilder
+	 * (e.g., "If a character has 2 or more magic, costs all of a
+	 * characters magic.").
+	 *
+	 * Returns the same htmlBuilder.
+	 */
+	addFullHtml:
+	/* Foundation.StringBuilder */ function(/* Foundation.StringBuilder */ htmlBuilder) {
+	    throw "unimplemented";
+	},
+	/*
+	 * Determines whether a cost can have a positive component to
+	 * it (i.e., usually, truly a cost).
+	 * 
+	 * Note: all four possible combos of isPositive and isNegative
+	 * are reasonable.  Both true: mixed; both false: free.
+	 */
+	isPositive:
+	/* Boolean */ function() { throw "unimplemented"; },
+	/*
+	 * Determines whether a cost can have a negative component to
+	 * it (i.e., usually, actually a benefit).
+	 * 
+	 * Note: all four possible combos of isPositive and isNegative
+	 * are reasonable.  Both true: mixed; both false: free.
+	 */
+	isNegative:
+	/* Boolean */ function() { throw "unimplemented"; },
+	/*
+	 * Determines whether a cost can be compared to another
+	 * cost. Note, however, that two comparable resources may
+	 * still not dominate in either direction or domination may be
+	 * situational (e.g., if one costs 2 magic and the other 1
+	 * magic and 1 gold or if one can cost 1+ magic up to all of
+	 * it and the other costs 2 magic).
+	 *
+	 * If it is, then it can be compared resource-by-resource; if
+	 * not, then it should not be considered to dominate or be
+	 * dominated by any resource.
+	 */
+	isComparable:
+	/* Boolean */ function() { throw "unimplemented"; },
+	/*
+	 * Produce true if this dominates cost. I.e., this cost is
+	 * weakly more expensive than (really "at least as expensive
+	 * as") cost along all dimensions.
+	 *
+	 * This will always be false if either cost is
+	 * incomparable. If two costs dominate each other, they are
+	 * equal. This comparison takes ranges into account (and so
+	 * will not return true (in the base implementation) either
+	 * way for two things that both cost 1--3 magic, since the
+	 * conditions under which they take on different costs may
+	 * differ).
+	 *
+	 * (Best to test against true (... === true), since we may
+	 * decide later to have some other return value on false?)
+	 */
+	dominates:
+	/* Boolean */ function(/* Cost */ cost)
 	{
+	    // TODO: test!!!
 	    if (!this.isComparable() || !cost.isComparable())
 		return false;
 	    
-	    // TODO: could be more efficient, but probably not important
 	    var thisResources = this.getResourceTypes();
 	    var thatResources = cost.getResourceTypes();
 	    for (var i = 0; i < thisResources.length; i++)
@@ -394,156 +481,189 @@ Foundation.createClass(
 	    }
 	    return true;
 	},
-	getMin:function(resource) { }, // the minimum cost in that resource (zero if not listed)
-	getMax:function(resource) { }, // the maximum cost in that resource (zero if not listed; can be infinite??)
-	getResourceTypes:function() { } // an array of all the types of resources relevant to this cost
+	/*
+	 * The minimum cost in that resource (zero if not listed).
+	 */
+	getMin:
+	/* Integer */ function(/* Resource */ resource) { throw "unimplemented"; },
+	/*
+	 * The maximum cost in that resource (zero if not listed; can be infinite??)
+	 */
+	getMax:
+	/* Integer */ function(/* Resource */ resource) { throw "unimplemented"; },
+	/*
+	 * An array of all the types of resources relevant to this cost.
+	 */
+	getResourceTypes:
+	/* Array<Resource> */ function() { throw "unimplemented"; }
     },
     {
 	// Static methods
     }
 );
 
-// // Foundation.createClass(
-// //     "GamesByEmail.Data.SimpleCost",
-// //     GamesByEmail.Data.Cost,
-// //     // mandatoryResources: an array of records with type (Resource)
-// //     // and count (Integer) fields; if the character cannot pay exactly
-// //     // the amount listed for each resource, then the cost cannot be
-// //     // paid
-// //     //
-// //     // uptoResources: an array as above; the cost consumes up to this
-// //     // much (on top of any mandatory resources) when paid.
-// //     //
-// //     // PRECONDITION: It is (currently) undefined what happens when a
-// //     // mandatoryResource and an uptoResource for the same resource
-// //     // have opposite signs.
-// //     function(mandatoryResources,uptoResources)
-// //     {
-// // 	if (mandatoryResources === undefined)
-// // 	    this.mandatoryResources = [];
-// // 	else if (typeof mandatoryResources.slice === "function")
-// // 	    this.mandatoryResources = mandatoryResources.slice();
-// // 	else
-// // 	    throw "mandatoryResources must be an array of objects with type:Resource and count:Integer";
-// // 	if (uptoResources === undefined)
-// // 	    this.uptoResources = [];
-// // 	else if (typeof uptoResources.slice === "function")
-// // 	    this.uptoResources = uptoResources.slice();
-// // 	else
-// // 	    throw "uptoResources must be an array of objects with type:Resource and count:Integer";
+/*
+ * A "simple" cost in the game, one that can be described, for each
+ * resource that participates in the cost, as a mandatory component
+ * (required to pass "canPay") and an "upTo" cost. IF the player can
+ * pay all the mandatory costs, then the actual amount paid will be
+ * the upTo cost, clipped to the player's maximum/minimum range.
+ */
+Foundation.createClass(
+    "GamesByEmail.Data.SimpleCost",
+    null,
+    GamesByEmail.Data.Cost,
+    /*
+     * Constructor. A simple cost has two parts, the mandatory
+     * resources that must be available to be paid (an object where
+     * each field is the name of a canonical resource, paired with the
+     * amount of the mandatory cost), and a similar object describing
+     * the actual cost if the mandatory amount is possible. Note that
+     * the values of these two are somewhat independent, i.e., for a
+     * given resource either can be larger than the other.
+     *
+     * If one is present without the other (e.g., a mandatory cost but
+     * no upto cost for gold), the other defaults to 0. (A 0 mandatory
+     * and non-0 upto cost means something that can be used---for that
+     * resource---regardless of the player's current levels, e.g.,
+     * magical breeze is "usable" regardless of how much
+     * magic/willpower the player currently has. A 0 upto cost and
+     * non-0 mandatory cost is a cost that requires some minimum
+     * availability along the given resource but then has no effect on
+     * that resource (e.g., the Giant Scorpion requires minimum magic
+     * (?) in order to gain gold but then has no effect on the
+     * player's amount of magic.)
+     */
+    /* Parameters: Map<String -> Integer>, Map<String -> Integer> */
+    function(mandatoryResources,uptoResources)
+    {
+	// TODO from here!!! Implementation of class overall must be
+	// slightly tweaked to match the description above.
+	if (mandatoryResources === undefined)
+	    this.mandatoryResources = [];
+	else if (typeof mandatoryResources.slice === "function")
+	    this.mandatoryResources = mandatoryResources.slice();
+	else
+	    throw "mandatoryResources must be an array of objects with type:Resource and count:Integer";
+	if (uptoResources === undefined)
+	    this.uptoResources = [];
+	else if (typeof uptoResources.slice === "function")
+	    this.uptoResources = uptoResources.slice();
+	else
+	    throw "uptoResources must be an array of objects with type:Resource and count:Integer";
 
-// // 	// TODO: check preconditions?
-// //     },
-// //     {
-// // 	// Virtual methods
-// // 	canPay:function(character) 
-// // 	{
-// // 	    for (var i = 0; i < this.mandatoryResources.length; i++)
-// // 	    {
-// // 		var type = this.mandatoryResources[i].type;
-// // 		var amt = this.mandatoryResources[i].count;
-// // 		var newLevel = character.getResource(type) + amt;
-// // 		if (!(character.getHardResourceMin(type) <= newLevel &&
-// // 		      newLevel <= character.getHardResourceMax(type)))
-// // 		    return type;
-// // 	    }
-// // 	    // Note: do NOT check uptoResources, since they cannot cause failure to pay.
-// // 	    return true;
-// // 	},
-// // 	pay:function(character)
-// // 	{
-// // 	    for (var i = 0; i < this.mandatoryResources.length; i++)
-// // 	    {
-// // 		var type = this.mandatoryResources[i].type;
-// // 		var amt = this.mandatoryResources[i].count;
-// // 		character.addResource(type, amt);
-// // 	    }
-// // 	    for (var i = 0; i < this.uptoResources.length; i++)
-// // 	    {
-// // 		var type = this.uptoResources[i].type;
-// // 		var amt = this.uptoResources[i].count;
-// // 		character.addResource(type, amt); // Let the character figure out how much can actually be added.
-// // 	    }
-// // 	    return true;
-// // 	},
-// // 	addSummaryHtml:function(htmlBuilder)
-// // 	{
-// // 	    // TODO!!!
-// // 	},
-// // 	addFullHtml:function(htmlBuilder)
-// // 	{
-// // 	    // TODO!!!
-// // 	},
-// // 	isPositive:function()
-// // 	{
-// // 	    for (var i = 0; i < this.mandatoryResources.length; i++)
-// // 	    {
-// // 		var type = this.mandatoryResources[i].type;
-// // 		var amt = this.mandatoryResources[i].count;
-// // 		if (amt > 0)
-// // 		    return true;
-// // 	    }
-// // 	    // TODO: consider what should happen if sign of mandatory
-// // 	    // and upto is not the same for a particular resource.
-// // 	    for (var i = 0; i < this.uptoResources.length; i++)
-// // 	    {
-// // 		var type = this.uptoResources[i].type;
-// // 		var amt = this.uptoResources[i].count;
-// // 		if (amt > 0)
-// // 		    return true;
-// // 	    }
-// // 	    return false;
-// // 	},
-// // 	isNegative:function()
-// // 	{
-// // 	    for (var i = 0; i < this.mandatoryResources.length; i++)
-// // 	    {
-// // 		var type = this.mandatoryResources[i].type;
-// // 		var amt = this.mandatoryResources[i].count;
-// // 		if (amt < 0)
-// // 		    return true;
-// // 	    }
-// // 	    for (var i = 0; i < this.uptoResources.length; i++)
-// // 	    {
-// // 		var type = this.uptoResources[i].type;
-// // 		var amt = this.uptoResources[i].count;
-// // 		if (amt < 0)
-// // 		    return true;
-// // 	    }
-// // 	    return false;
-// // 	},
-// // 	isComparable:function()
-// // 	{
-// // 	    return true;
-// // 	},
-// // 	getMin:function(resource)
-// // 	{
-// // 	    var mandatory = 0;
-// // 	    for (var i = 0; i < this.mandatoryResources.length; i++)
-// // 		if (this.mandatoryResources[i].type == resource)
-// // 		    mandatory = this.mandatoryResources[i].count;
-// // 	    var upto = 0;
-// // 	    for (var i = 0; i < this.uptoResources.length; i++)
-// // 		if (this.uptoResources[i].type == resource)
-// // 		    upto = this.uptoResources[i].count;
-// // 	    return min(mandatory, mandatory + upto);
-// // 	},
-// // 	getMax:function(resource)
-// // 	{
-// // 	    var mandatory = 0;
-// // 	    for (var i = 0; i < this.mandatoryResources.length; i++)
-// // 		if (this.mandatoryResources[i].type == resource)
-// // 		    mandatory = this.mandatoryResources[i].count;
-// // 	    var upto = 0;
-// // 	    for (var i = 0; i < this.uptoResources.length; i++)
-// // 		if (this.uptoResources[i].type == resource)
-// // 		    upto = this.uptoResources[i].count;
-// // 	    return max(mandatory, mandatory + upto);
-// // 	},
-// //     },
-// //     {
-// // 	// Static methods
-// //     }
-// // );
+	// TODO: check preconditions?
+    },
+    {
+	// Virtual methods
+	canPay:function(character) 
+	{
+	    for (var i = 0; i < this.mandatoryResources.length; i++)
+	    {
+		var type = this.mandatoryResources[i].type;
+		var amt = this.mandatoryResources[i].count;
+		var newLevel = character.getResource(type) + amt;
+		if (!(character.getHardResourceMin(type) <= newLevel &&
+		      newLevel <= character.getHardResourceMax(type)))
+		    return type;
+	    }
+	    // Note: do NOT check uptoResources, since they cannot cause failure to pay.
+	    return true;
+	},
+	pay:function(character)
+	{
+	    for (var i = 0; i < this.mandatoryResources.length; i++)
+	    {
+		var type = this.mandatoryResources[i].type;
+		var amt = this.mandatoryResources[i].count;
+		character.addResource(type, amt);
+	    }
+	    for (var i = 0; i < this.uptoResources.length; i++)
+	    {
+		var type = this.uptoResources[i].type;
+		var amt = this.uptoResources[i].count;
+		character.addResource(type, amt); // Let the character figure out how much can actually be added.
+	    }
+	    return true;
+	},
+	addSummaryHtml:function(htmlBuilder)
+	{
+	    // TODO!!!
+	},
+	addFullHtml:function(htmlBuilder)
+	{
+	    // TODO!!!
+	},
+	isPositive:function()
+	{
+	    for (var i = 0; i < this.mandatoryResources.length; i++)
+	    {
+		var type = this.mandatoryResources[i].type;
+		var amt = this.mandatoryResources[i].count;
+		if (amt > 0)
+		    return true;
+	    }
+	    // TODO: consider what should happen if sign of mandatory
+	    // and upto is not the same for a particular resource.
+	    for (var i = 0; i < this.uptoResources.length; i++)
+	    {
+		var type = this.uptoResources[i].type;
+		var amt = this.uptoResources[i].count;
+		if (amt > 0)
+		    return true;
+	    }
+	    return false;
+	},
+	isNegative:function()
+	{
+	    for (var i = 0; i < this.mandatoryResources.length; i++)
+	    {
+		var type = this.mandatoryResources[i].type;
+		var amt = this.mandatoryResources[i].count;
+		if (amt < 0)
+		    return true;
+	    }
+	    for (var i = 0; i < this.uptoResources.length; i++)
+	    {
+		var type = this.uptoResources[i].type;
+		var amt = this.uptoResources[i].count;
+		if (amt < 0)
+		    return true;
+	    }
+	    return false;
+	},
+	isComparable:function()
+	{
+	    return true;
+	},
+	getMin:function(resource)
+	{
+	    var mandatory = 0;
+	    for (var i = 0; i < this.mandatoryResources.length; i++)
+		if (this.mandatoryResources[i].type == resource)
+		    mandatory = this.mandatoryResources[i].count;
+	    var upto = 0;
+	    for (var i = 0; i < this.uptoResources.length; i++)
+		if (this.uptoResources[i].type == resource)
+		    upto = this.uptoResources[i].count;
+	    return min(mandatory, mandatory + upto);
+	},
+	getMax:function(resource)
+	{
+	    var mandatory = 0;
+	    for (var i = 0; i < this.mandatoryResources.length; i++)
+		if (this.mandatoryResources[i].type == resource)
+		    mandatory = this.mandatoryResources[i].count;
+	    var upto = 0;
+	    for (var i = 0; i < this.uptoResources.length; i++)
+		if (this.uptoResources[i].type == resource)
+		    upto = this.uptoResources[i].count;
+	    return max(mandatory, mandatory + upto);
+	},
+    },
+    {
+	// Static methods
+    }
+);
 
 
